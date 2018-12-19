17f00ba71b30460c30b2889c1e7d15803da1f58c
Alexander <savaleks@gmail.com>
2018-12-19 10:31:17 +0200

    Loaded Static Resources

---
+++ online-shop/src/main/java/com/savaleks/onlineshop/controller/PageController.java
@@ -0,0 +1,39 @@
+package com.savaleks.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	// == request adding at the end String ==
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView modview = new ModelAndView("page");
+		modview.addObject("greeting", "Welcome to Spring MVC");
+		return modview;
+	}
+
+	// == request without adding at the end String(default value) ==
+//	@RequestMapping(value = "/test")
+//	public ModelAndView index(@RequestParam(value = "greeting", required = false) String greeting) {
+//		if (greeting == null) {
+//			greeting = "Hello there friend!";
+//		}
+//		ModelAndView modview = new ModelAndView("page");
+//		modview.addObject("greeting", greeting);
+//		return modview;
+//	}
+
+//	// == request with adding at the address end String, dynamic request  ==
+//	@RequestMapping(value = "/test/{greeting}")
+//	public ModelAndView index(@PathVariable("greeting") String greeting) {
+//		if (greeting == null) {
+//			greeting = "Hello there friend!";
+//		}
+//		ModelAndView modview = new ModelAndView("page");
+//		modview.addObject("greeting", greeting);
+//		return modview;
+//	}
+}
